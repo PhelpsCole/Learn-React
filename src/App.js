4d23f3656ef76da6f3f6e105900b12d98e757c2d
PhelpsCole <adrmsk@mail.ru>
2022-09-06 00:50:58 +0300

    Two types of input

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import {useRef} from 'react';
 import Counter from "./components/Counter";
 import ClassCounter from "./components/ClassCounter";
 import PostItem from "./components/PostItem";
@@ -20,19 +21,28 @@ function App() {
             {id: 4, title: "Rust", body: "C++ - лучший язык программирования"},
         ])
     const [title, setTytle] = useState('')
+    const bodyInputRef = useRef()
     const addNewPost = (e) => {
         e.preventDefault()
         console.log(title)
+        console.log(bodyInputRef.current.value)
     }
     return (
         <div className="App">
             <form>
+                {/*Управляемый компонент*/}
                 <MyInput
                     value={title}
                     onChange = {e => setTytle(e.target.value)}
                     type="text"
-                    placeholder="Название поста"/>
-                <MyInput type="text" placeholder="Описание поста"/>
+                    placeholder="Название поста"
+                />
+                {/*Неуправляемый компонент*/}
+                <MyInput
+                    ref={bodyInputRef}
+                    type="text"
+                    placeholder="Описание поста"
+                />
                 <MyButton onClick={addNewPost}>Создать пост</MyButton>
             </form>
             <PostList title="Список постов 2" posts={posts2}/>
